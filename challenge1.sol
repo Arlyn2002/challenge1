da508eba40ca2022649d7a6ca563d0be120cf46e
Arlyn Gracia <128880835+Arlyn2002@users.noreply.github.com>
2024-05-13 15:05:35 +0800

    Create challenge1.sol

---
+++ challenge1.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+
+contract Challenge_1 {
+    uint num;
+    string name;
+    bool isFalse;
+    int negNum;
+
+    function setNegNum(int _negNum) public {
+        negNum = _negNum;
+    }
+    
+    function getNegnum() public view returns (int) {
+        return negNum;
+    }
+
+    function setNum(uint _num) public {
+        num = _num;
+    }
+
+    function getNum() public view returns (uint) {
+        return num;
+    }
+    
+    function setName(string memory _name) public {
+        name = _name;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function setBool(bool _isFalse) public {
+        isFalse = _isFalse;
+    }
+
+    function getBool() public view returns (bool){
+        return isFalse;
+    }
+}
+
